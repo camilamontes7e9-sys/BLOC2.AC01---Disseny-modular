648c0093a97f1b1423e00edd5b53267c1920cde3
Camila.Montes <camila.montes.7e9@itb.cat>
2025-11-27 13:00:17 +0100

    Test of exercise two

--- Program.cs
+++ Program.cs
@@ -7,48 +7,104 @@ namespace Program
     {
         public static void Main() 
         {
-            const string InitialMsg = "This program will tell if a integer number is in a range";
-            const string InputMsg = "Enter a number: ";
+            const string InitialMsg = "This program will convert a decimal number for a temperature to another temperature system";
+            const string ChooseOp = "Choose an option to convert: ";
+            const string OptionOne = "1-From Celsius to Fahrenheit";
+            const string OptionTwo = "2-From Fahrenheit to Celcius";
+            const string OptionTree = "3-From Celcius to Kelvin";
+            const string ErrorMsg = "Error, enter a valid input.";
+            const string EnterDecimal = "Enter the decimal number to convert it";
 
             Console.WriteLine(InitialMsg);
+            Console.WriteLine(ChooseOp);
+            Console.WriteLine(OptionOne);
+            Console.WriteLine(OptionTwo);
+            Console.WriteLine(OptionTree);
+            Console.WriteLine(OptionOne);
 
-            int num1, minValue1, maxValue1;
+            int input;
+            float decimalNumberTwo;
 
-            Console.WriteLine(InputMsg);
-            num1 = Convert.ToInt32(Console.ReadLine());
+            try
+            {
+                input = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (Exception)
+            {
+                Console.WriteLine(ErrorMsg);
+            }
 
-            minValue1 = 5;
-            maxValue1 = 35;
 
-            InRange(num1, minValue1, maxValue1);
-
+            switch (input)
+            {
+                case 1:
+                    Console.WriteLine(EnterDecimal);
+                    try
+                    {
+                        decimalNumberTwo = Convert.ToSingle(Console.ReadLine());
+                    }
+                    catch
+                    {
+                        Console.WriteLine(ErrorMsg);
+                    }
+                    ConvertTemperature(decimalNumberTwo);
+                    break;
+                case 2:
+                    Console.WriteLine(EnterDecimal);
+                    try
+                    {
+                        decimalNumberTwo = Convert.ToSingle(Console.ReadLine());
+                    }
+                    catch
+                    {
+                        Console.WriteLine(ErrorMsg);
+                    }
+                    ConvertTemperature(decimalNumberTwo);
+                    break;
+                case 3:
+                    Console.WriteLine(EnterDecimal);
+                    try
+                    {
+                        decimalNumberTwo = Convert.ToSingle(Console.ReadLine());
+                    }
+                    catch
+                    {
+                        Console.WriteLine(ErrorMsg);
+                    }
+                    ConvertTemperature(decimalNumberTwo);
+                    break;
+            }
            
         }
-
-        public static bool InRange(int num, int minValue, int maxValue)
+        public static float ConvertTemperature(float decimalNumberTwo)
         {
-            //5-35
-            const string IsSmaller = "The number it's out of the range, it's smaller than the minimum number of the range.";
-            const string IsBigger = "The number it's out of the range, it's bigger than the maximum number of the range.";
-            const string IsInRangeMsg ="Congrats! Your number is in the range!";
-            bool isInRange;
-            if (num < minValue) 
+            const string CelciusToFahrenheit = "You converted {0}Cº to {1}ºF";
+            const string FahrenheitToCelcius = "You conveted {0}ºF to {1}Cº";
+            const string CelciusToKelvin = "You converted {0}Cº to {1}K";
+            
+            int input;
+            float decimalNumber;
+            float secondNum = decimalNumber;
+
+            switch (input)
             {
-                Console.WriteLine(IsSmaller);
-                isInRange = false;
+                case 1:
+                    decimalNumber = decimalNumber * 9 / 5 + 32;
+                    Math.Round(decimalNumber, 2);
+                    Console.WriteLine(CelciusToFahrenheit, secondNum, decimalNumber);
+                    break;
+                case 2:
+                    decimalNumber = (decimalNumber - 32) * 5 / 9;
+                    Math.Round(decimalNumber, 2);
+                    Console.WriteLine(FahrenheitToCelcius, secondNum, decimalNumber);
+                    break;
+                case 3:
+                    decimalNumber = decimalNumber + 273.15f;
+                    Math.Round(decimalNumber, 2);
+                    Console.WriteLine(CelciusToKelvin, secondNum, decimalNumber);
+                    break;
             }
-            else if (num > maxValue)
-            {
-                Console.WriteLine(IsBigger);
-                isInRange = false;
-            }
-            else
-            {
-                Console.WriteLine(IsInRangeMsg);
-                isInRange = true;
-            }
-            return isInRange;
+            return decimalNumber;
         }
-       
     }
 }
